eb7be97852001197b0138dd41eed6feec5bffba8
Mansour Aria <mansour.aria96@hotmail.com>
2024-07-18 18:32:26 -0400

    added code for conditional statements

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,59 @@
+//defined variables
+let userRole = "admin";
+let accessLevel;
+
+// if...else block by assigning different roles
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Acess Level:", accessLevel);
+
+// Defining Variables and user Nested if...else for LoggedIn and userMessage
+// defined variables
+
+let isLoggedIn = true;
+let userMessage;
+
+// Nested if...else for isLoggedIn and userMessage
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!"
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+// Defining variables and switch statement for userType and userCategory
+// Defined Variables
+
+let userType = "subscriber";
+let userCategory;
+
+// Switch statement for userType and userCategory
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unkown";
+}
+
+console.log("User Category", userCategory);
