896ba2b0760b35e4d7701ccdcb7368b2a18578b4
Mansour-AR <mansour.aria96@hotmail.com>
2024-07-19 23:37:33 +0200

    Added codes to count Instagram followers

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -10,15 +10,26 @@ function increaseCount() {
     checkCountValue(); //Check count value and display messages
 
 }
-
+// Function to display counts
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count;
 }
-
+// Function to check count value
 function checkCountValue() {
     if (count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
     }
+}
+// Function to reset count
+
+function resetCount() {
+
+ if (count != 0) {
+    count = 0;
+    document.getElementById('countDisplay').innerHTML=count;
+    alert("Followers count has been reset.");
+ }
+
 }
