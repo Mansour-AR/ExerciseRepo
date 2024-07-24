fec1ad2701ec2d2519a2f42e65febc04c719be84
Mansour Aria <mansour.aria96@hotmail.com>
2024-07-24 10:10:23 -0400

    Added code to perform arithmetic operations and debug the code using debugger

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -9,10 +9,12 @@ function performOperation() {
 
     if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
-             let result = multiply(num1, num2);
+             let result1 = multiply(num1, num2);
+             let result2 = addition(num1, num2);
+             let result3 = division(num1, num2);
 
              // Display the result
-             displayResult(result);
+             displayResult(result1, result2, result3);
     } else {
         displayResult('Please enter valid numbers');
     }
@@ -25,9 +27,27 @@ function performOperation() {
         return a * b;
     }
 
-    function displayResult(result) {
+    function addition(a,b) {
+        // Introduce a debugger statement to pause execution 
+        debugger;
+
+        // Add the numbers
+        return a + b;
+    }
+
+    function division(a,b) {
+        // Introduce a debugger statement to pause execution
+        debugger;
+
+        // Divide the numbers
+        return a / b;
+
+    }
+
+    function displayResult(result1, result2, result3) {
         // Display the result in the paragraph element
         const resultElement = document.getElementById('result');
-        resultElement.textContent = `The result is: ${result}`;
+        resultElement.textContent = `Addition Result: ${result2} ` +
+        `Multiplication Result: ${result1} ` + `Divistion Result: ${result3}`;
     }
 }
