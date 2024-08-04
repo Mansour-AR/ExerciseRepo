5d9c503cbca8b0cb90d00154c7574578c20de540
Mansour Aria <mansour.aria96@gmail.com>
2024-08-04 13:14:01 -0400

    added files and codes to fetch weather details

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,27 @@
+// Function to show weather details
+
+function showweatherDetails(event) {
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = 'd762628c3d5e4371025e64ad00fa5b8d'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+             .then(response => response.json())
+             .then(data => {
+                const weatherInfo = document.getElementById('weatherInfo');
+                weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                        <p>Temperature: ${data.main.temp} &#8451</p>
+                                        <p>Weather: ${data.weather[0].description}</p>`;
+             })
+
+             .catch(error => {
+                      console.error('Error fetching weather:', error);
+                      const weatherInfo = document.getElementById('weatherInfo');
+                      weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again</p>`;
+             });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
