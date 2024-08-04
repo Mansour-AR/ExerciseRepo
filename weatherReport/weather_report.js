d4928098a90c39543143ff1f6fc61a79e60e01f9
Mansour Aria <mansour.aria96@hotmail.com>
2024-08-04 17:38:53 -0400

    added files and codes to fetch weather report

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -3,17 +3,70 @@
 function showweatherDetails(event) {
     event.preventDefault();
 
-    const city = document.getElementById('city').value;
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
     const apiKey = 'd762628c3d5e4371025e64ad00fa5b8d'; 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     fetch(apiUrl)
              .then(response => response.json())
              .then(data => {
                 const weatherInfo = document.getElementById('weatherInfo');
-                weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                                        <p>Temperature: ${data.main.temp} &#8451</p>
-                                        <p>Weather: ${data.weather[0].description}</p>`;
+                weatherInfo.innerHTML = `
+                                        <tr>
+                                        <th scope="row">Longitude of the location</th>
+                                        <td colspan="3">${data.coord.lon}</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Latitude of the location</th>
+                                        <td colspan="3">${data.coord.lat}</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Temperature</th>
+                                        <td colspan="3">${data.main.temp} &deg;F
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Humidity, %</th>
+                                        <td colspan="3">${data.main.humidity} &deg;F
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Minimum temperature at the moment</th>
+                                        <td colspan="3"> ${data.main.temp_min} &deg;F
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Maximum temperature at the moment</th>
+                                        <td colspan="3">${data.main.temp_max} &deg;F
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Atmospheric pressure on the sea level, hPa</th>
+                                        <td colspan="3">${data.main.sea_level}</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Atmospheric pressure on the ground level, hPa</th>
+                                        <td colspan="3">${data.main.grnd_level}</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Wind Speed</th>
+                                        <td colspan="3">${data.wind.speed}</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Wind Direction</th>
+                                        <td colspan="3">${data.wind.deg}</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">Cloudiness, %</th>
+                                        <td colspan="3">${data.clouds.all}</td>
+                                        </tr>
+                                        <tr>
+                                        <tr>
+                                        <th scope="row">Country Code</th>
+                                        <td colspan="3">${data.sys.country}</td>
+                                        </tr>`;
+                                        
              })
 
              .catch(error => {
